87b94937def9ace0a1f856a70de9454141110b74
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-03 04:43:30 +0500

    Updates

---
+++ precache-manifest.a5ba7bd24b8b7d23896222e847826056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe00aa51072e5f42c9d1871d59bcf85",
+    "url": "/SnapShots/index.html"
+  },
+  {
+    "revision": "6da4d10a44a27c32e547",
+    "url": "/SnapShots/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "d181d5d7af02efcd3dab",
+    "url": "/SnapShots/static/js/2.c619e43e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnapShots/static/js/2.c619e43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da4d10a44a27c32e547",
+    "url": "/SnapShots/static/js/main.3c3997b9.chunk.js"
+  },
+  {
+    "revision": "7ca3f3c55a86ba256ffb",
+    "url": "/SnapShots/static/js/runtime-main.05380363.js"
+  }
+]);
